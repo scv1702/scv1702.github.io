5a78e2223303e27fcb383b86cb2085e8463e5879
SHIN CHANGYU <scv1702@naver.com>
2021-03-08 11:49:00 +0900

    Ajax 기능 추가

---
+++ js/fetch.js
@@ -0,0 +1,36 @@
+function fetchPage(name) {
+    fetch(name).then(function(response) {
+        response.text().then(function(text) {
+            document.querySelector('article').innerHTML = text;
+        })
+    })
+}
+
+function toUpperStr(str) {
+    var _str = '';
+    _str = str.charAt(0).toUpperCase() + str.slice(1);
+    return _str;
+}
+
+fetch('list').then(function(response) {
+    response.text().then(function(text) {
+    var items = text.split(',');
+    var i = 0;
+    var tags = '';
+    while(i < items.length) {
+        var item = items[i]
+        item = item.trim() // 개행문자 제거
+        item = toUpperStr(item);
+        var tag = '<li><a href="#!' + item + '" onclick="fetchPage(\'' + item + '\')">' + item + '</a></li>'
+        tags = tags + tag;
+        i = i + 1;
+    }
+    document.querySelector('#list').innerHTML = tags;
+    })
+});
+
+if (location.hash) {
+    fetchPage(location.hash.substr(2));
+} else if (location.hash) {
+    fetchPage('welcome');
+}
